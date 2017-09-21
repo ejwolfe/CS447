1b0e9c059058d87ccd2cdc3a9d1652fc8b9b5a58
Eric Wolfe <rojowolf21@gmail.com>
2017-09-21 18:47:03 -0500

    Trying to fix request for file issues

--- project1/proxyWindows.cpp
+++ project1/proxyWindows.cpp
@@ -1,12 +1,15 @@
 #include <stdio.h>          // Needed for printf()
 #include <stdlib.h>         // Needed for exit()
-#include <string.h>         // Needed for strcpy() and strlen()
+#include <string>         // Needed for strcpy() and strlen()
 #include <fcntl.h>          // Needed for file i/o constants
 #include <sys/stat.h>       // Needed for file i/o constants
 #include <stddef.h>         // Needed for _threadid
 #include <process.h>        // Needed for _beginthread() and _endthread()
 #include <io.h>             // Needed for open(), close(), and eof()
 #include <windows.h>        // Needed for all Winsock stuff
+#include <vector>
+
+using namespace std;
 
 // Defines
 #define PROXYPORT 9080
@@ -22,19 +25,19 @@
 // Prototypes
 void clientToServer(void *in_args); //Thread for sending information from the client to the server
 void serverToClient(void *in_args); //Thread for sending information from the server to the client
-unsigned int clientSocket, serverSocket;
-
-using namespace std;
+//unsigned int clientSocket, serverSocket;
+vector<unsigned int> clientSocket;
+vector<unsigned int> serverSocket;
 
 int main(int argc, char const *argv[]) {
 	printf("This is the proxy\n");
 	WORD wVersionRequested = MAKEWORD(1, 1);
 	WSADATA wsaData;
 	WSAStartup(wVersionRequested, &wsaData);
-	unsigned int        proxySocket, serverSocketConnection;
+	unsigned int        proxySocket, serverSocketConnection, clientSocketTemp, serverSocketTemp;
 	struct sockaddr_in  proxyAddress, clientAddress, serverAddress;
 	struct in_addr      clientIPAddress, serverIPAddress;
-	int                 addressLength;
+	int                 addressLength, clientSocketPosition = 0, serverSocketPosition = 0;;
 
 	// Create a socket, fill-in address information, bind it, and then listen
 	proxySocket = socket(AF_INET, SOCK_STREAM, 0);
@@ -48,10 +51,12 @@ int main(int argc, char const *argv[]) {
 
 	while (1) {
 		addressLength = sizeof(clientAddress);
-		if ((clientSocket = accept(proxySocket, (struct sockaddr *)&clientAddress, &addressLength)) == 0) {
+		if ((clientSocketTemp = accept(proxySocket, (struct sockaddr *)&clientAddress, &addressLength)) == 0) {
 			printf("ERROR - Unable to create socket \n");
 			exit(1);
 		}
+		clientSocket.push_back(clientSocketTemp);
+
 		/* Display the signature of the new client ----- */
 
 		memcpy(&clientIPAddress, &clientAddress.sin_addr.s_addr, 4);
@@ -61,14 +66,15 @@ int main(int argc, char const *argv[]) {
 		printf("\n");
 
 		// Create the server socket, fill-in address information, and then connect
-		serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+		serverSocketTemp = socket(AF_INET, SOCK_STREAM, 0);
 		serverAddress.sin_family = AF_INET;
 		serverAddress.sin_port = htons(SERVERPORT);
 		serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
-		serverSocketConnection = connect(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
+		connect(serverSocketTemp, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
+		serverSocket.push_back(serverSocketTemp);
 
+		int threadArgs[2] = { clientSocketPosition, serverSocketPosition };
 		// Setting up arguments for the threads (easier to pass socket information) and then start threads
-		unsigned int threadArgs[2] = { clientSocket, serverSocket };
 		if (_beginthread(clientToServer, 4096, (void *)threadArgs) < 0) {
 			printf("ERROR - Unable to create client to server thread \n");
 			exit(1);
@@ -77,6 +83,8 @@ int main(int argc, char const *argv[]) {
 			printf("ERROR - Unable to create client to server thread \n");
 			exit(1);
 		}
+		clientSocketPosition++;
+		serverSocketPosition++;
 	}
 	closesocket(proxySocket);
 	WSACleanup();
@@ -89,41 +97,28 @@ void clientToServer(void *in_args) {
 	unsigned int  fh, bufferLength, returnCode;
 	char          inBuffer[BUFFERSIZE], outBuffer[BUFFERSIZE], *fileName;
 	bool          connectionFlag = 1;
-	returnCode = recv(clientSocket, inBuffer, BUFFERSIZE, 0);
+	int	clientSocketPosition, serverSocketPosition;
 
-	//printf("The return code is %d and the in buffer says %s", returnCode, inBuffer);
-	if (returnCode != -1) {
-		if (serverSocket == 0) {
-			strcpy(outBuffer, NOTOK_404);
-			send(clientSocket, outBuffer, strlen(outBuffer), 0);
-			strcpy(outBuffer, MESS_404);
-			send(clientSocket, outBuffer, strlen(outBuffer), 0);
-			connectionFlag = 0;
-		}
-		else {
-			printf("%s", inBuffer);
-			strcpy(outBuffer, inBuffer);
-			send(serverSocket, outBuffer, strlen(outBuffer), 0);
-		}
-	}
-	_endthread();
-}
+	clientSocketPosition = ((int *) in_args)[0];
+	serverSocketPosition = ((int *) in_args)[1];
 
-void serverToClient(void *in_args) {
-	unsigned int  fh, bufferLength, returnCode;
-	char          inBuffer[BUFFERSIZE], outBuffer[BUFFERSIZE], *fileName;
-	bool          connectionFlag = 1;
+	returnCode = recv(clientSocket[clientSocketPosition], inBuffer, strlen(inBuffer), 0);
 	while (1) {
-		/* Clean up the buffers */
-		for (int j = 0; j < BUFFERSIZE; j++)
-		{
-			outBuffer[j] = '\0';
-			inBuffer[j] = '\0';
+		//printf("The return code is %d and the in buffer says %s", returnCode, inBuffer);
+		if (returnCode != -1) {
+			if (serverSocket[serverSocketPosition] == 0) {
+				strcpy(outBuffer, NOTOK_404);
+				send(clientSocket[clientSocketPosition], outBuffer, strlen(outBuffer), 0);
+				strcpy(outBuffer, MESS_404);
+				send(clientSocket[clientSocketPosition], outBuffer, strlen(outBuffer), 0);
+				connectionFlag = 0;
+			}
+			else {
+				//printf("%s", inBuffer);
+				strcpy(outBuffer, inBuffer);
+				send(serverSocket[serverSocketPosition], outBuffer, strlen(outBuffer), 0);
+			}
 		}
-		returnCode = recv(serverSocket, in Buffer, BUFFERSIZE, 0);
-		send(clientSocket, inBuffer, strlen(inBuffer), 0);
 	}
-	closesocket(serverSocket);
-	closesocket(clientSocket);
 	_endthread();
 }
